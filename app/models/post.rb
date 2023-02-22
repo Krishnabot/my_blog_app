232176e4443d50113127f5f86eff0cc0e01412c5
Krishna <libraryismyparadise@gmail.com>
2023-02-22 11:53:10 +0545

    Add a method that updates the posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
     has_many :comments, foreign_key: 'post_id'
     has_many :likes
     belongs_to :user, class_name: 'User'
+    after_create :update_counter
+
+    def upadate_countr 
+        if !user.posts_countr 
+            user.posts_counter = 1
+            user.save 
+        else
+            user.posts_counter +=1
+            user.save
+        end
+    end
+
+
 end
