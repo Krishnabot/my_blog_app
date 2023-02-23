0539494589b057c9e59393f06c0eeaf689a498ec
Krishna <libraryismyparadise@gmail.com>
2023-02-23 13:58:18 +0545

    Add rspec test fo Model Like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :Post
+  belongs_to :post
   after_save :update_likes_counter
 
   private
+
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-
   before do
     @user = User.create(name: 'Halle', photo: 'Photo', bio: 'Ruby Developper', posts_counter: 0)
-    @post = Post.create(author: @user, title: 'Hello', text: 'Hello Rails', comments_counter: 0, likes_counter: 0)
-    @like = Like.create(post: @post, author: @user)
+    @post = Post.create(user: @user, title: 'Hello', text: 'Hello Rails', comments_counter: 0, likes_counter: 0)
+    @like = Like.create(post: @post, user: @user)
   end
 
   it 'Should updates the likes_counter' do
     @like.send(:update_likes_counter)
-    expect(@post.reload.likesCounter).to eq(1)
+    expect(@post.reload.likes_counter).to eq(1)
   end
 end
