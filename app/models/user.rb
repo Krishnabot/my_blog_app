451e50c71140390f791bdd0532652665340af243
Krishna <libraryismyparadise@gmail.com>
2023-02-23 13:58:40 +0545

    Add rspec test fo Model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0, message: "must be an integer greater than or equal to zero"}
+  validates :posts_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'must be an integer greater than or equal to zero' }
 
   def last_3_posts
     posts.order(created_at: :desc).limit(3)
