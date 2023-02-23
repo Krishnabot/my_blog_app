2c72911151793e73b141c2ab81aa0bf1ae5ffc10
Krishna <libraryismyparadise@gmail.com>
2023-02-23 13:58:30 +0545

    Add rspec test fo Model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,21 +2,19 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes
   belongs_to :user, class_name: 'User'
-  validates :title, presence: true, length: { maximum: 250}
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true}
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true}
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-   def most_recent_comments
+  def five_most_recent_comments
     comments.limit(5).order(created_at: :desc)
   end
 
   after_save :update_posts_counter
 
-
   private
 
-  def upadate_post_counter
-  author.update(posts_counter: author.posts.count)
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
   end
-
 end
---
+++ spec/model/post_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+  before do
+    @user = User.create(name: 'Halle', photo: 'Photo', bio: 'Ruby Developper', posts_counter: 0)
+    @post = Post.create(user: @user, title: 'Hello', text: 'Hello Rails', comments_counter: 0, likes_counter: 0)
+    @comments = []
+  end
+
+  it 'Should be present and <= 250 characters' do
+    @post.title = 'Hello'
+    expect(@post).to be_valid
+  end
+
+  it 'comments_counter should be an integer and sould be  >= 0 ' do
+    @post.comments_counter = 0
+    expect(@post).to be_valid
+  end
+
+  it 'likes_counter should be an integer and sould be  >= 0' do
+    @post.likes_counter = 0
+    expect(@post).to be_valid
+  end
+
+  it 'should return the five most recent comments' do
+    result = @post.five_most_recent_comments
+    expect(result).to eq(@comments)
+  end
+
+  it 'Should updates the posts_counter' do
+    @post.send(:update_posts_counter)
+    expect(@user.reload.posts_counter).to eq(1)
+  end
+end
