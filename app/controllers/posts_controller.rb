7bd1ed3bdd55006bb21293acc3588ea81935a7a3
Krishna <libraryismyparadise@gmail.com>
2023-03-08 13:31:27 +0545

    Fix N+1 problems for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(user_id: params[:user_id])
+    @posts = Post.includes(:user).where(user_id: params[:user_id])
   end
 
   def show
