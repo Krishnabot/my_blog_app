81f1de6dcfef5c690549958614277733a720f904
Krishna <libraryismyparadise@gmail.com>
2023-03-08 21:16:29 +0545

    Modify the views in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  devise_for :users
   # Defines the root path route ("/")
-   root to "users#index"
+   root to: "users#index"
 end
