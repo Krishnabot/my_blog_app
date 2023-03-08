1add92354d85c2ac469f3fec8c9b881c0c966feb
jloman <jloman200@gmail.com>
2023-03-08 14:29:55 +0300

    Linters config

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 RSpec.describe 'Index page', type: :feature do
-    before(:each) do
-        @user = User.create(
-          name: 'John',
-          bio: 'Software Developer',
-          photo: 'http://hello1.com/org.png',
-          posts_counter: 0
-        )
-        @user_two = User.create(
-          name: 'Victor',
-          bio: 'Graphic designer',
-          photo: 'http://hello1.com/org.png',
-          posts_counter: 0
-        )
-        visit "/users"
-      end
-      describe 'index page' do
-        it 'shows the right content' do
-          expect(page).to have_content('John')
-          expect(page).to have_content('Victor')
-        end
-        it 'should have the profile picture' do
-            expect(page).to have_content(@user.photo)
-            expect(page).to have_content(@user_two.photo)
-        end
-        it 'should redirect to users show page' do
-            visit users_path(@user.id)
-            click_on 'show user', match: :first
-            expect(page).to have_current_path user_path([@user.id])
-        end
-      end
-
+  before(:each) do
+    @user = User.create(
+      name: 'John',
+      bio: 'Software Developer',
+      photo: 'http://hello1.com/org.png',
+      posts_counter: 0
+    )
+    @user_two = User.create(
+      name: 'Victor',
+      bio: 'Graphic designer',
+      photo: 'http://hello1.com/org.png',
+      posts_counter: 0
+    )
+    visit '/users'
+  end
+  describe 'index page' do
+    it 'shows the right content' do
+      expect(page).to have_content('John')
+      expect(page).to have_content('Victor')
+    end
+    it 'should have the profile picture' do
+      expect(page).to have_content(@user.photo)
+      expect(page).to have_content(@user_two.photo)
+    end
+    it 'should redirect to users show page' do
+      visit users_path(@user.id)
+      click_on 'show user', match: :first
+      expect(page).to have_current_path user_path([@user.id])
+    end
+  end
 end
--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -8,30 +8,30 @@ RSpec.describe 'User', type: :feature do
       posts_counter: 0
     )
   end
-    describe 'show page' do
-        it "has users's username." do
-          visit "/users/#{@user.id}"
-            expect(page).to have_content(@user.name)
-        end
-        it "has users's bio." do
-          visit "/users/#{@user.id}"
-            expect(page).to have_content(@user.bio)
-        end
-        it 'should have the profile picture' do
-          visit "/users/#{@user.id}"
-            expect(page).to have_content(@user.photo)
-        end
-        it 'has link to all posts' do
-          visit "/users/#{@user.id}"
-          expect(page).to have_link('See all posts')
-        end
-        it 'has recent three posts' do
-          visit "/users/#{@user.id}"
-          expect(page).to have_selector('.user-no-of-posts', count: 1)
-        end
-        it 'has users number of posts' do
-          visit "/users/#{@user.id}"
-          expect(page).to have_content("Number of posts: #{@user.posts_counter}")
-        end
+  describe 'show page' do
+    it "has users's username." do
+      visit "/users/#{@user.id}"
+      expect(page).to have_content(@user.name)
     end
+    it "has users's bio." do
+      visit "/users/#{@user.id}"
+      expect(page).to have_content(@user.bio)
+    end
+    it 'should have the profile picture' do
+      visit "/users/#{@user.id}"
+      expect(page).to have_content(@user.photo)
+    end
+    it 'has link to all posts' do
+      visit "/users/#{@user.id}"
+      expect(page).to have_link('See all posts')
+    end
+    it 'has recent three posts' do
+      visit "/users/#{@user.id}"
+      expect(page).to have_selector('.user-no-of-posts', count: 1)
+    end
+    it 'has users number of posts' do
+      visit "/users/#{@user.id}"
+      expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+    end
+  end
 end
