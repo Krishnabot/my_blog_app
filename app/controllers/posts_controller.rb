2b0999cf71d5351fa4c594b104e853b08ae6e41e
Krishna <libraryismyparadise@gmail.com>
2023-03-08 19:27:34 +0545

    Fix N + 1 Problem for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.includes(:user).where(user_id: params[:user_id])
-  end
+def index
+  @user = User.includes(:posts).find(params[:user_id])
+  @posts = @user.posts.includes(:user)
+end
 
   def show
     @post = Post.find(params[:id])
