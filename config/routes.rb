847c8163bf6cffe32a0d7b0948e7402d0742a2a8
Krishna <libraryismyparadise@gmail.com>
2023-03-08 20:43:38 +0545

    Set up and configure Device

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+ devise_for :users
   # Defines the root path route ("/")
-  # root "users#index"
+   root to "users#index"
 end
