a602b4552fafb3228919c94375d7b7bc2624c5d5
Krishna <libraryismyparadise@gmail.com>
2023-03-09 16:44:57 +0545

    Add a role column to the users table by  Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_152138) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_105908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,8 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_152138) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_152138) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -69,5 +70,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_152138) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "users_id"
 end
